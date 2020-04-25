63a682044cf89e360675f9941e4b94481c66a87e
Germey <cqc@cuiqingcai.com>
2020-04-25 22:47:29 +0800

    add

---
+++ spider1.py
@@ -0,0 +1,29 @@
+import requests
+from urllib.parse import urljoin
+import json
+
+BASE_URL = 'https://login3.scrape.cuiqingcai.com/'
+LOGIN_URL = urljoin(BASE_URL, '/api/login')
+INDEX_URL = urljoin(BASE_URL, '/api/book')
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+response_login = requests.post(LOGIN_URL, json={
+    'username': USERNAME,
+    'password': PASSWORD
+})
+data = response_login.json()
+print('Response JSON', data)
+jwt = data.get('token')
+print('JWT', jwt)
+
+headers = {
+    'Authorization': f'jwt {jwt}'
+}
+response_index = requests.get(INDEX_URL, params={
+    'limit': 18,
+    'offset': 0
+}, headers=headers)
+print('Response Status', response_index.status_code)
+print('Response URL', response_index.url)
+print('Response Data', response_index.json())
